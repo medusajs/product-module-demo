d67c6f5929ba195ef08bd552b04c55a8e52cdfb5
adrien2p <adrien.deperetti@gmail.com>
2023-05-17 17:42:50 +0200

    cleanup webpack

--- next.config.js
+++ next.config.js
@@ -8,23 +8,7 @@ const nextConfig = {
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
   ) => {
     config.externals.push({
-      "@mikro-orm/seeder": "@mikro-orm/seeder",
-      '@mikro-orm/mongodb': '@mikro-orm/mongodb',
-      '@mikro-orm/mysql': '@mikro-orm/mysql',
-      '@mikro-orm/better-sqlite': '@mikro-orm/better-sqlite',
-      '@mikro-orm/migrations': '@mikro-orm/migrations',
-      '@mikro-orm/entity-generator': '@mikro-orm/entity-generator',
-      '@mikro-orm/mariadb': '@mikro-orm/mariadb',
-      '@mikro-orm/sqlite': '@mikro-orm/sqlite',
-      '@vscode/sqlite3': '@vscode/sqlite3',
-      'sqlite3': 'sqlite3',
-      'better-sqlite3': 'better-sqlite3',
-      'mysql': 'mysql',
-      'mysql2': 'mysql2',
-      'oracledb': 'oracledb',
-      'pg-native': 'pg-native',
-      'pg-query-stream': 'pg-query-stream',
-      'tedious': 'tedious',
+      "knex": "knex",
       "@medusajs/product": "@medusajs/product",
     })
     return config;
