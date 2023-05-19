daddf86eb37e70a1d464cd74b24a207c5f54230a
adrien2p <adrien.deperetti@gmail.com>
2023-05-19 15:58:48 +0200

    WIP: product fetching

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -6,20 +6,56 @@ declare global {
   var productModule: any
 }
 
-export async function GET() {
+export async function GET(request: Request) {
+  const localisation = (request.headers.get("X-localisation") || "Denmark").toLowerCase()
+  const recentCategoryIds = (request.headers.get("X-recent-categories") || "").split(",").map((category) => category.toLowerCase())
+
   global.productModule = global.productModule ?? await ProductModuleInitialize({
     database: {
       clientUrl: process.env.POSTGRES_URL!,
       schema: "public",
+      driverOptions: {
+        connection: {
+          ssl: {
+            rejectUnauthorized: false
+          }
+        },
+      }
     }
   })
 
-  console.log(productModule)
+  const products = await queryProducts({ localisation, recentCategoryIds })
+  return NextResponse.json({ products });
+}
 
-  const data = await productModule.list({
-    tags: { value: ["France"] }
+async function queryProducts({ localisation, recentCategoryIds }: { localisation: string, recentCategoryIds: string[] }): Promise<any[]> {
+  const limit = 12
+
+  // Fetch product for the categories and outside the categories paginated by 12
+  const promises: Promise<any>[] = []
+
+  promises.push(productModule.list({
+    tags: { value: [localisation] },
+    categories: { id: recentCategoryIds }
   }, {
-    relations: ["tags"]
-  })
-  return NextResponse.json({ data });
+    relations: ["tags", "categories"],
+    take: limit
+  }))
+
+  promises.push(await productModule.list({
+    tags: { value: [localisation] },
+    categories: { id: { $nin: { recentCategoryIds }}}
+  }, {
+    relations: ["tags", "categories"],
+    take: limit
+  }))
+
+  const [recentProducts, otherProducts] = await Promise.all(promises)
+  const products = recentProducts
+
+  if (products.length < limit) {
+    products.push(...otherProducts.slice(0, limit - products.length))
+  }
+
+  return products
 }
