f153f8f6f0f044d1cc840f2851cdef8578363dca
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-19 13:16:47 +0200

    chore: allow unauth ssl in driverOptions

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,25 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { initialize as ProductModuleInitialize } from "@medusajs/product"
-import { NextResponse } from 'next/server';
+import { initialize as ProductModuleInitialize } from "@medusajs/product";
+import { NextResponse } from "next/server";
 
 declare global {
-  var productModule: any
+  var productModule: any;
 }
 
 export async function GET() {
-  global.productModule = global.productModule ?? await ProductModuleInitialize({
-    database: {
-      clientUrl: process.env.POSTGRES_URL!,
-      schema: "public",
+  global.productModule =
+    global.productModule ??
+    (await ProductModuleInitialize({
+      database: {
+        clientUrl: process.env.POSTGRES_URL!,
+        schema: "public",
+        driverOptions: {
+          connection: {
+            ssl: {
+              rejectUnauthorized: false,
+            },
+          },
+        },
+      },
+    }));
+
+  console.log(productModule);
+
+  const data = await productModule.list(
+    {
+      tags: { value: ["France"] },
+    },
+    {
+      relations: ["tags"],
     }
-  })
-
-  console.log(productModule)
-
-  const data = await productModule.list({
-    tags: { value: ["France"] }
-  }, {
-    relations: ["tags"]
-  })
+  );
   return NextResponse.json({ data });
 }
