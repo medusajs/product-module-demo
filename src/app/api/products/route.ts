d14fa234d35ca65eca319e5a8f9e2c1b085d8d58
adrien2p <adrien.deperetti@gmail.com>
2023-05-26 11:16:59 +0200

    cleanup function

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -15,11 +15,56 @@ declare global {
   var productService: ProductTypes.IProductService;
 }
 
+type Data = {
+  handle?: string;
+  categoryId?: string;
+  categoryName?: string;
+  continent: string;
+  continentText: { article: string; name: string };
+  country: string;
+}
+
 export async function GET(req: NextRequest) {
   const productService = (global.productService ??=
     await ProductModuleInitialize());
 
-  const handle = req.nextUrl.searchParams.get("handle");
+  const {
+    handle,
+    categoryId,
+    categoryName,
+    continent,
+    continentText,
+    country
+  } = await getData(req);
+
+  let [personalizedProducts, allProducts] = await queryProducts({
+    handle,
+    continent
+  });
+
+  await getAndAssignPricesToProducts({ products: allProducts })
+
+  const data = orderProductByCategoryIdFirst({
+    products: allProducts,
+    personalizedProducts,
+    groupByCategoryId: categoryId,
+  })
+
+  return NextResponse.json({
+    personalized_section: {
+      country,
+      continent_text: continentText,
+      products: data.personalizedProducts,
+    },
+    all_products_section: {
+      category_name: categoryName,
+      products: data.products,
+    },
+  });
+}
+
+async function getData(req: NextRequest): Promise<Data> {
+  const handle = req.nextUrl.searchParams.get("handle") ?? undefined;
 
   const userId = req.cookies.get("userId")?.value;
   let categoryId, categoryName;
@@ -39,13 +84,31 @@ export async function GET(req: NextRequest) {
   let { name: country, continent } = isoAlpha2Countries[countryCode];
   const continentText = formatContinent(continent);
 
+  return {
+    handle,
+    country,
+    categoryId,
+    categoryName,
+    continent,
+    continentText,
+  }
+}
+
+async function queryProducts({
+ handle,
+ continent
+}: {
+  handle?: string;
+  continent: string
+}): Promise<[ProductTypes.ProductDTO[], ProductTypes.ProductDTO[]]> {
+  const productService = global.productService
   const filters: { handle?: string } = {};
 
   if (handle) {
     filters.handle = handle;
   }
 
-  let [personalizedProducts, allProducts] = await Promise.all([
+  return await Promise.all([
     productService.list(
       {
         ...filters,
@@ -61,12 +124,14 @@ export async function GET(req: NextRequest) {
       order: { id: "DESC" },
     }),
   ]);
+}
 
+async function getAndAssignPricesToProducts({ products }: { products: ProductTypes.ProductDTO[] }): Promise<void> {
   const region = await client.regions.list().then((res) => res.regions[0]);
 
   const pricedProducts = (
     await client.products.list({
-      id: allProducts.map((p: ProductTypes.ProductDTO) => p.id),
+      id: products.map((p) => p.id),
       expand: "variants,variants.prices,tags",
       region_id: region.id ?? undefined,
     })
@@ -80,10 +145,7 @@ export async function GET(req: NextRequest) {
     variantsMap.set(variant.id!, variant);
   }
 
-  const productMap = new Map<string, ProductTypes.ProductDTO>();
-  const categoryProductsMap = new Map<string, ProductTypes.ProductDTO[]>();
-
-  for (const product of allProducts) {
+  for (const product of products) {
     (product as any).variants = (product as any).variants.map(
       (variant: any) => {
         return {
@@ -92,7 +154,22 @@ export async function GET(req: NextRequest) {
         };
       }
     );
+  }
+}
 
+function orderProductByCategoryIdFirst({
+  products,
+  personalizedProducts,
+  groupByCategoryId
+}: {
+  products: ProductTypes.ProductDTO[];
+  personalizedProducts: ProductTypes.ProductDTO[];
+  groupByCategoryId?: string;
+}) {
+  const productMap = new Map<string, ProductTypes.ProductDTO>();
+  const categoryProductsMap = new Map<string, ProductTypes.ProductDTO[]>();
+
+  for (const product of products) {
     const category = product.categories[0];
     if (!categoryProductsMap.has(category?.id)) {
       categoryProductsMap.set(category?.id, []);
@@ -103,30 +180,21 @@ export async function GET(req: NextRequest) {
   }
 
   let recentlyViewedProducts: ProductTypes.ProductDTO[] = [];
-  if (categoryId) {
-    recentlyViewedProducts = categoryProductsMap.get(categoryId)!;
-    categoryProductsMap.delete(categoryId);
+  if (groupByCategoryId) {
+    recentlyViewedProducts = categoryProductsMap.get(groupByCategoryId)!;
+    categoryProductsMap.delete(groupByCategoryId);
   }
 
-  allProducts = Array.from(recentlyViewedProducts.values()).concat(
+  const allProducts = Array.from(recentlyViewedProducts.values()).concat(
     Array.from(categoryProductsMap.values()).flat()
   );
 
+  // Assign the products data to the light personalized products
   personalizedProducts = personalizedProducts.map(
     (p: ProductTypes.ProductDTO) => {
       return productMap.get(p.id)!;
     }
   );
 
-  return NextResponse.json({
-    personalized_section: {
-      country,
-      continent_text: continentText,
-      products: personalizedProducts,
-    },
-    all_products_section: {
-      category_name: categoryName,
-      products: allProducts,
-    },
-  });
+  return { personalizedProducts, products }
 }
