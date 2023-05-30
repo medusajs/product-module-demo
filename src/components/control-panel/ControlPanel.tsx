a33c29ca44220315a3688d8fd08f5a15392ee3ea
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-30 12:45:23 +0200

    feat: hover modals

--- src/components/control-panel/ControlPanel.tsx
+++ src/components/control-panel/ControlPanel.tsx
@@ -5,24 +5,22 @@ import { LastClick } from "../icons/lastclick";
 import { ArrowRefresh } from "../icons/arrow-refresh";
 import { Button } from "../common";
 import CountryPicker from "./CountryPicker";
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import HoverModal from "./HoverModal";
 
 type Props = {
   data: PersonalizationData | null;
   selectedCountry?: string;
-  isLoading: boolean;
   loadingTime: number;
   setCountry: (country: Country | null) => void;
 };
 
-export default function ControlPanel({
-  data,
-  isLoading,
-  loadingTime,
-  setCountry,
-}: Props) {
-  if (isLoading) return null;
+export default function ControlPanel({ data, loadingTime, setCountry }: Props) {
   if (!data) return null;
+
+  const [locationHover, setLocationHover] = useState(false);
+  const [recentItemHover, setRecentItemHover] = useState(false);
+  const [resetHover, setResetHover] = useState(false);
 
   const { country } = data.personalized_section;
   const { category_name } = data.all_products_section;
@@ -59,24 +57,69 @@ export default function ControlPanel({
   }, [onKeyDown]);
 
   return (
-    <div className="flex justify-center fixed left-0 right-0 bottom-5 z-50">
+    <div className="flex flex-col justify-center items-center fixed left-0 right-0 bottom-5 z-50">
       <div className="flex flex-row justify-between gap-x-4 w-fit h-fit p-4 text-labels-regular font-medium shadow-card-hover-light dark:shadow-card-hover-dark rounded-2xl bg-base-light dark:bg-base-dark">
-        <CountryPicker
-          country={country}
-          setCountry={setCountry}
-          inputRef={inputRef}
-        />
-        <Button>
-          <LastClick />
-          <span className="text-subtle-light dark:text-subtle-dark">
-            Last clicked:
-          </span>
-          <span>{category_name}</span>
-        </Button>
-        <Button onClick={reset}>
-          <ArrowRefresh />
-          <div>Reset</div>
-        </Button>
+        <div
+          onMouseEnter={() => setLocationHover(true)}
+          onMouseLeave={() => setLocationHover(false)}
+          className="relative flex flex-col justify-center items-center"
+        >
+          {locationHover && (
+            <HoverModal
+              className="flex flex-row items-center justify-between gap-x-2 w-auto"
+              tag="L"
+            >
+              Location
+            </HoverModal>
+          )}
+          <CountryPicker
+            country={country}
+            setCountry={setCountry}
+            inputRef={inputRef}
+          />
+        </div>
+        <div
+          className="relative flex flex-col justify-center items-center"
+          onMouseEnter={() => setRecentItemHover(true)}
+          onMouseLeave={() => setRecentItemHover(false)}
+        >
+          {recentItemHover && (
+            <HoverModal className="w-[350px] text-center">
+              Most Recently Viewed Product:
+              <span className="text-base-light dark:text-base-dark">
+                {" "}
+                {category_name}.
+                <br />
+              </span>
+              We use this to personalize the product grid
+            </HoverModal>
+          )}
+          <Button>
+            <LastClick />
+            <span className="text-subtle-light dark:text-subtle-dark">
+              Last clicked:
+            </span>
+            <span>{category_name}</span>
+          </Button>
+        </div>
+        <div
+          onMouseEnter={() => setResetHover(true)}
+          onMouseLeave={() => setResetHover(false)}
+          className="relative flex flex-col justify-center items-center"
+        >
+          {resetHover && (
+            <HoverModal
+              className="flex flex-row items-center justify-between gap-x-2 w-auto"
+              tag="R"
+            >
+              Reset
+            </HoverModal>
+          )}
+          <Button onClick={reset}>
+            <ArrowRefresh />
+            <div>Reset</div>
+          </Button>
+        </div>
         <div className="flex flex-row items-center gap-2 border-solid border-l border-neutral-button-light dark:border-neutral-button-dark px-3 py-0 h-fit self-center">
           <span className="text-subtle-light dark:text-subtle-dark ">
             Loading:
@@ -84,6 +127,7 @@ export default function ControlPanel({
           {loadingTime}ms
         </div>
       </div>
+      {/* </div> */}
     </div>
   );
 }
---
+++ src/components/control-panel/HoverModal.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { PropsWithChildren } from "react";
+import { Tag } from "../common";
+
+type Props = PropsWithChildren & {
+  tag?: string;
+  className: string;
+};
+
+const HoverModal = ({ tag, className, children }: Props) => {
+  return (
+    <div
+      className={clsx(
+        className,
+        "absolute bottom-16 h-fit p-3 text-subtle-light dark:text-subtle-dark font-medium shadow-card-hover-light dark:shadow-card-hover-dark rounded-2xl bg-base-light dark:bg-base-dark"
+      )}
+    >
+      {children}
+      {tag && <Tag>{tag}</Tag>}
+    </div>
+  );
+};
+
+export default HoverModal;
