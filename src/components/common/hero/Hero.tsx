1cf4035d47a410be7e897893c8c73611777dc492
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-30 16:34:07 +0200

    fix: proper hero bg texture

--- src/components/common/hero/Hero.tsx
+++ src/components/common/hero/Hero.tsx
@@ -4,7 +4,7 @@ import { Medusa } from "@/components/icons/medusa";
 
 const Hero = () => {
   return (
-    <div className="relative h-96 w-full rounded-xl overflow-hidden bg-[url('/hero.svg')] bg-cover">
+    <div className="relative h-96 w-full overflow-hidden bg-[url('/hero.svg')] bg-cover bg-base-light dark:bg-base-dark shadow-card-hover-light dark:shadow-card-hover-dark rounded-2xl">
       <div className="absolute inset-0 flex flex-col gap-6 items-center justify-center">
         <div className="flex flex-col gap-2 items-center justify-center">
           <div className="text-white text-4xl">Product Module Demo.</div>
