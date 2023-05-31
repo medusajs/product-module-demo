08b425c38c0f7c2d5f5baaa7ccace96bd9fbc705
Victor Gerbrands <victorgerbrands@gmail.com>
2023-05-31 15:08:01 +0200

    chore: fix merge issues

--- src/components/common/button/Button.tsx
+++ src/components/common/button/Button.tsx
@@ -4,9 +4,15 @@ import { PropsWithChildren } from "react";
 type Props = PropsWithChildren<{
   variant?: "primary" | "inverted";
   onClick?: () => void;
+  className: string;
 }>;
 
-const Button = ({ variant = "primary", onClick, children }: Props) => {
+const Button = ({
+  variant = "primary",
+  onClick,
+  className,
+  children,
+}: Props) => {
   const bg = {
     primary:
       "bg-gradient-to-b from-white dark:from-[#2E2E32] to-[#F8F9FA] dark:to-[#28282C] text-base-light dark:text-base-dark border-neutral-button-light dark:border-neutral-button-dark",
@@ -18,7 +24,7 @@ const Button = ({ variant = "primary", onClick, children }: Props) => {
     <button
       onClick={onClick}
       className={clsx(
-        props.className,
+        className,
         bg,
         "flex flex-row items-center justify-center gap-1 p-2 text-labels-regular font-medium h-fit py-[9px] rounded-[7px] border"
       )}
