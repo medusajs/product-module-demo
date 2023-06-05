6091d6061e311a4304915b000d4a037684ea8f25
Victor Gerbrands <victorgerbrands@gmail.com>
2023-06-05 22:14:38 +0200

    feat: homepage responsive

--- src/components/layout/nav/Nav.tsx
+++ src/components/layout/nav/Nav.tsx
@@ -1,15 +1,11 @@
-import {
-  CombinationMarkLogo,
-  MagnifyingGlass,
-  ShoppingBag,
-} from "@/components/icons";
+import { CombinationMarkLogo, ShoppingBag } from "@/components/icons";
 import Cart from "@/components/cart";
 import Link from "next/link";
 import { Suspense } from "react";
 
 const Nav = async () => {
   return (
-    <nav className="flex items-center justify-center bg-base-light dark:bg-base-dark h-[73px] border-b border-base-light dark:border-base-dark">
+    <nav className="flex items-center justify-center bg-base-light dark:bg-base-dark h-[73px] border-b border-base-light dark:border-base-dark px-4">
       <div className="flex items-center justify-between w-full max-w-7xl">
         <div className="text-icon-base-light dark:text-icon-base-dark">
           <Link href="/">
