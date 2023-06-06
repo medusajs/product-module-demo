0faad2fc151aa58e2ecd1532c45e985843891e96
fPolic <frane@medusajs.com>
2023-06-06 15:53:00 +0200

    fix: notification responsive positioning

--- src/components/common/notification/Notification.tsx
+++ src/components/common/notification/Notification.tsx
@@ -13,7 +13,7 @@ type Props = {
 export default function Notification({ type, title, body, onClose }: Props) {
   return (
     <div className="fixed w-screen h-screen z-[9999]">
-      <div className="fixed h-fit w-fit bottom-6 right-6 flex rounded-lg bg-white dark:bg-base-dark dark:shadow-card-hover-dark p-5 animate-fadeIn z-[9999]">
+      <div className="fixed h-fit w-fit bottom-6 md:right-6 right-0 mx-2 sm:mx-0 flex rounded-lg bg-white dark:bg-base-dark dark:shadow-card-hover-dark p-5 animate-fadeIn z-[9999]">
         <div className="flex flex-col">
           <div className="flex flex-row justify-between gap-5">
             {type === "success" ? (
