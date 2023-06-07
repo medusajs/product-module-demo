f1b158b1a842bef2c905018bfd86870da727b3ce
fPolic <mainacc.polic@gmail.com>
2023-06-07 15:22:00 +0200

    fix: control panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,6 @@ export default function Home() {
             data={data}
             loadingTime={loadingTime}
             setCountry={setCountry}
-            scrollToFeatures={scrollToFeatures}
           />
         </div>
       </div>
