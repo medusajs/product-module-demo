582920c9801df3a414b3857c90de529c42603507
Victor Gerbrands <victorgerbrands@gmail.com>
2023-06-07 15:46:37 +0200

    feat: about modal

---
+++ src/components/common/custom-list-item/CustomListItem.tsx
@@ -0,0 +1,25 @@
+import { Circle } from "../../icons/circle";
+
+type CustomListItemProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const CustomListItem: React.FC<CustomListItemProps> = ({ title, children }) => (
+  <li className="flex h-full relative">
+    <div className="flex flex-col items-center w-6 mr-4">
+      <Circle fill="#1c1c1c" className="z-10 mt-1" />
+      <div className="h-full mt-1 w-[1px] bg-[#2E2E32] absolute top-0 z-0"></div>
+    </div>
+    <div className="flex flex-col gap-y-2 h-full pl-12 z-40 mb-8">
+      <span className="text-tag-purple text-labels-small font-medium">
+        {title}
+      </span>
+      <div className="text-subtle-light dark:text-subtle-dark text-body-regular">
+        {children}
+      </div>
+    </div>
+  </li>
+);
+
+export default CustomListItem;
