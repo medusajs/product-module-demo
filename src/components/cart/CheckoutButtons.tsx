875621416269344dc7cd02aa94b3e9a0a15856a6
fPolic <mainacc.polic@gmail.com>
2023-06-09 12:45:46 +0200

    fix: cart buttons responsivenes

--- src/components/cart/CheckoutButtons.tsx
+++ src/components/cart/CheckoutButtons.tsx
@@ -8,15 +8,23 @@ const CheckoutButtons = () => {
   const { showNotification, hideNotification } = useNotifications();
 
   return (
-    <div className="border-t border-[#2E2E32] p-8 flex flex-row justify-between gap-4 z-0">
+    <div className="border-t border-[#2E2E32] p-8 flex flex-col md:flex-row justify-between gap-4 z-0">
       <Button
-        className="w-1/2"
+        className="md:w-1/2 w-100"
         variant="inverted"
         onClick={() =>
           showNotification(
             "error",
             "This action is not part of the demo.",
-            <span>You can learn how to set up payment plugins with Medusa <a className="font-bold cursor-pointer" href="https://docs.medusajs.com/plugins/payment">here</a></span>,
+            <span>
+              You can learn how to set up payment plugins with Medusa{" "}
+              <a
+                className="font-bold cursor-pointer"
+                href="https://docs.medusajs.com/plugins/payment"
+              >
+                here
+              </a>
+            </span>,
             hideNotification
           )
         }
@@ -24,13 +32,21 @@ const CheckoutButtons = () => {
         Pay with Apple Pay
       </Button>
       <Button
-        className="w-1/2"
+        className="md:w-1/2 w-100"
         variant="primary"
         onClick={() =>
           showNotification(
             "error",
             "This action is not part of the demo.",
-            <span>You can learn how to set up payment plugins with Medusa <a className="font-bold cursor-pointer" href="https://docs.medusajs.com/plugins/payment">here</a></span>,
+            <span>
+              You can learn how to set up payment plugins with Medusa{" "}
+              <a
+                className="font-bold cursor-pointer"
+                href="https://docs.medusajs.com/plugins/payment"
+              >
+                here
+              </a>
+            </span>,
             hideNotification
           )
         }
