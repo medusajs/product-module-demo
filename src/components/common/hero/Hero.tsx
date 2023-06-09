1895c4c3ec3dfd2038b60663d989f42acaf2264f
fPolic <mainacc.polic@gmail.com>
2023-06-09 13:12:41 +0200

    fix: Hero responsive tweaks

--- src/components/common/hero/Hero.tsx
+++ src/components/common/hero/Hero.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="relative h-96 w-full overflow-hidden bg-[url('/hero.svg')] bg-cover bg-base-light dark:bg-base-dark shadow-card-hover-light dark:shadow-card-hover-dark rounded-2xl">
+    <div className="relative h-96 w-full overflow-hidden bg-[url('/hero.svg')] bg-cover bg-base-light dark:bg-base-dark shadow-card-hover-light dark:shadow-card-hover-dark rounded-2xl p-2">
       <div className="absolute inset-0 flex flex-col gap-6 items-center justify-center">
         <div className="flex flex-col gap-2 items-center justify-center text-center">
-          <div className="text-base-light dark:text-base-dark text-4xl">
+          <div className="text-base-light dark:text-base-dark text-3xl md:text-4xl">
             Product Module Demo.
           </div>
-          <div className="text-subtle-light dark:text-subtle-dark text-4xl">
+          <div className="text-subtle-light dark:text-subtle-dark text-3xl md:text-4xl">
             Powered by Medusa.
           </div>
         </div>
