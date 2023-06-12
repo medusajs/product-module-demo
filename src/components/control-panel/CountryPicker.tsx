bb56355ea0ec23b755a678bc357cbcf42c6f01f3
fPolic <mainacc.polic@gmail.com>
2023-06-12 16:24:07 +0200

    fix: zIndex on country picker

--- src/components/control-panel/CountryPicker.tsx
+++ src/components/control-panel/CountryPicker.tsx
@@ -37,7 +37,7 @@ export default function CountryPicker({ country, setCountry }: Props) {
   return (
     <div className="w-[100%] lg:w-60 h-fit">
       <Combobox value={country} onChange={(value: any) => setCountry(value)}>
-        <div className="relative">
+        <div className="relative z-40">
           <div className="flex flex-row gap-2 text-labels-regular font-medium rounded-[7px] border bg-gradient-to-b from-white dark:from-[#2E2E32] to-[#F8F9FA] dark:to-[#28282C] text-base-light dark:text-base-dark border-neutral-button-light dark:border-neutral-button-dark">
             <Combobox.Button className="flex items-center justify-center flex-row w-full border-none pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0 rounded border bg-gradient-to-b from-white dark:from-[#2E2E32] to-[#F8F9FA] dark:to-[#28282C] text-base-light dark:text-base-dark">
               <Globe />
