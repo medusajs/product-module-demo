175557d0b88bc32050c9ce2d188f13945cfab53a
Victor Gerbrands <victorgerbrands@gmail.com>
2023-06-13 17:05:11 +0200

    fix: disable hover on mobile

--- src/components/products/card/Thumbnail.tsx
+++ src/components/products/card/Thumbnail.tsx
@@ -8,11 +8,11 @@ type Props = {
 const Thumbnail = ({ thumbnail, alt = "" }: Props) => {
   return (
     <div className="w-full aspect-[121/72] relative bg-subtle-light dark:bg-subtle-dark overflow-hidden">
-      <div className="absolute inset-0 rounded-t-2xl flex opacity-0 group-focus-within/card:opacity-100 group-hover/card:opacity-100 bg-overlay-light dark:bg-overlay-dark z-10 items-center justify-center transition-all">
+      <div className="absolute inset-0 rounded-t-2xl flex opacity-0 group-focus-within/card:opacity-100 md:group-hover/card:opacity-100 bg-overlay-light dark:bg-overlay-dark z-10 items-center justify-center transition-all">
         <p>View Product</p>
       </div>
       {thumbnail && (
-        <div className="absolute inset-0 group-hover/card:blur-[2px] group-hover/card:scale-105 transition-transform ease-linear duration-300">
+        <div className="absolute inset-0 md:group-hover/card:blur-[2px] md:group-hover/card:scale-105 transition-transform ease-linear duration-300">
           <Image
             src={thumbnail}
             alt={alt}
