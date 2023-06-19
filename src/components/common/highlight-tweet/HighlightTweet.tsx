46f0b135a4e7e21b2b6039a8ca71484a179b2cc8
Victor Gerbrands <victorgerbrands@gmail.com>
2023-06-19 10:09:24 +0200

    feat: add tracking to like tweet

--- src/components/common/highlight-tweet/HighlightTweet.tsx
+++ src/components/common/highlight-tweet/HighlightTweet.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { Button } from "../button";
 import { Twitter } from "@/components/icons";
 
 const HighlightTweet = ({ id }: { id: string | undefined }) => {
   if (!id) return null;
+
+  const trackEvent = () => {
+    gtag("event", "like_tweet", {
+      page_path: window.location.pathname,
+      send_to: process.env.NEXT_PUBLIC_GA_ID,
+    });
+  };
 
   return (
     <div className="flex flex-row flex-wrap gap-4 justify-center items-center h-fit w-fit rounded-xl shadow-card-rest-dark p-4 mx-auto bg-base-dark">
@@ -15,7 +24,7 @@ const HighlightTweet = ({ id }: { id: string | undefined }) => {
         href={`https://twitter.com/intent/like?tweet_id=${id}`}
         target="_blank"
       >
-        <Button>
+        <Button onClick={trackEvent}>
           <Twitter />
           Like Tweet
         </Button>
